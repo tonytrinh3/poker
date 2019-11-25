fca19b9bc00b4ee82839a46c494de40606e576f7
Tony Trinh <tony.w.trinh3@gmail.com>
2019-11-24 21:54:10 -0800

    card array created

--- src/js/index.js
+++ src/js/index.js
@@ -25,16 +25,42 @@ const getDeckOfCards = async () => {
         
         const playerTwo= await(await fetch(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=5`)).json();
         
+
+        const handArray = (playersCards) => {
+            const cardsArray = playersCards.map((curEl)=>{
+              return pokerSolverConversion(curEl.code);
+              
+            })
+            return cardsArray;
+        };
+
+        const pokerSolverConversion = (cardCode) =>{
+            const splitCardCode= cardCode.split("");
+              let cardNumber = splitCardCode[0];
+              
+              if(cardNumber === "0"){
+                cardNumber = "T";
+              }
+          
+              const suit = splitCardCode[1].toLowerCase();
+
+              return cardNumber.concat(suit);
+        };
+    
+
         state.playerOneCards = playerOne.cards;
-        console.log(state.playerOneCards)
-            state.playerTwoCards = playerTwo.cards;
+        state.playerCardsArray__1 = handArray(state.playerOneCards);
+        console.log(state.playerCardsArray__1);
+       
+        state.playerTwoCards = playerTwo.cards;
+        state.playerCardsArray__2 = handArray(state.playerOneCards);
 
         //https://stackoverflow.com/questions/33846682/react-onclick-function-fires-on-render/33846747
         //Because you are calling that function instead of passing the function to onClick, change that line to this:
 
 
         let count = 0;
-        console.log(count);
+        
 
         
         const renderCard = (cards,playerNumber) =>{
@@ -54,21 +80,30 @@ const getDeckOfCards = async () => {
                 `;
                 document.head.appendChild(style);
                 //passed down the cards array from json, cardindex that is relevant to changing the card, and player number
-                renderNewCard(cards,cardIndex, playerNumber);
+                renderNewCard(cards,index, playerNumber);
 
             });
         };
 
             //receives the cards array from json, cardindex that is relevant to changing the card, and player number
-        const renderNewCard = (cards,index, playerNumber)=>{
+        const renderNewCard = (cards, index, playerNumber)=>{
             //render new card
-            document.querySelector(`.players-card__card--${index}`).addEventListener('click', async () =>{   
+            const cardIndex = (playerNumber*10)+index+1;
+            document.querySelector(`.players-card__card--${cardIndex}`).addEventListener('click', async () =>{   
                 count++
                 console.log(count);
                 const newCard = await(await fetch(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=1`)).json();
+                console.log(newCard);
                 const displayNewCard = `<img src = ${newCard.cards[0].image} alt = ${newCard.cards[0].code}>`;
-                const el = document.querySelector(`.players-card__card--${index}`);
-                    
+                const el = document.querySelector(`.players-card__card--${cardIndex}`);
+
+                if(playerNumber ===1){
+                    state.playerCardsArray__1.splice(index,1,pokerSolverConversion(newCard.cards[0].code));
+                    console.log(state.playerCardsArray__1);
+                } else if (playerNumber === 2){
+                    state.playerCardsArray__2.splice(index,1,pokerSolverConversion(newCard.cards[0].code));
+                }
+                
                 
                 //https://www.w3schools.com/jsref/met_node_removechild.asp
                 el.removeChild(el.childNodes[0]);
@@ -77,7 +112,7 @@ const getDeckOfCards = async () => {
                 //https://dev.to/karataev/set-css-styles-with-javascript-3nl5
                 let style = document.createElement('style');
                 style.innerHTML = `
-                .players-card__card--${index} {
+                .players-card__card--${cardIndex} {
                 background-color: red;
                 border-radius: 10px;
                 padding: 4px;
@@ -85,7 +120,9 @@ const getDeckOfCards = async () => {
                 `;
                 document.head.appendChild(style);
 
-                document.querySelector(`.players-card__card--${index}`).insertAdjacentHTML('beforeend', displayNewCard);
+                document.querySelector(`.players-card__card--${cardIndex}`).insertAdjacentHTML('beforeend', displayNewCard);
+
+
 
                                 
                 if (count === 3){
@@ -147,23 +184,8 @@ const getDeckOfCards = async () => {
             document.querySelector(`.turn__button`).disabled=true;
         });
 
-        const handSolve = (playersCards) => {
-            const cardsArray = playersCards.map((curEl)=>{
-              let codeArray = curEl.code.split("");
-              let cardNumber = codeArray[0];
-              
-              if(cardNumber === "0"){
-                cardNumber = "T";
-              }
-          
-              const suit = codeArray[1].toLowerCase();
-              return codeArray = cardNumber.concat(suit);
-              
-            })
-            return cardsArray;
-          }
-          
-         console.log(handSolve(playerOne.cards));
+        
+
 
         document.querySelector(`.showdown__button`).addEventListener('click', () =>{
             let style = document.createElement('style');
